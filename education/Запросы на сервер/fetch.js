ea88ba62e1dc3fafdaac9330845a42ca56cbdf51
LyubovTatur <71625191+LyubovTatur@users.noreply.github.com>
2022-04-23 17:16:42 +0300

    add error throw

--- education/Запросы на сервер/fetch.js
+++ education/Запросы на сервер/fetch.js
@@ -2,14 +2,19 @@ const requestUrl = 'https://jsonplaceholder.typicode.com/users'
 
 function sendRequest(method, url, body = null) {
     const headers = {
-        'Content-Type':'application/json'
+        'Content-Type': 'application/json'
     }
-    return fetch(url,{
+    return fetch(url, {
         method: method,
         body: JSON.stringify(body),
         headers: headers
-    }).then(response=>{
-        return response.json()
+    }).then(response => {
+        if (response.ok)
+            return response.json()
+        else
+            return response.json().then(err => {
+               throw new Error('что то пошло не так')
+            })
     })
 
 }
