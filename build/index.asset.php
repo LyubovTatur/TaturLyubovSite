b6f823853c1a33106646f79c39e5efc077bd4a86
LyubovTatur <lubatatur58@gmail.com>
2022-05-13 13:27:17 +0300

    pc commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element'), 'version' => 'abe95beba8fa880120122d875f6344e4');
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './main.scss'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import TestComponent from './TestComponent'
+
+
+ReactDOM.render(
+    <TestComponent/>,document.querySelector('#app')
+)
